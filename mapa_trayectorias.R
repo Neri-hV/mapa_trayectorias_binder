940f4d13b3bb6ac7fd7829a2fcb0cb02da01d9b6
Coco <91480391+Neri-hV@users.noreply.github.com>
2025-08-07 11:41:41 -0400

    Create mapa_trayectorias.R

---
+++ mapa_trayectorias.R
@@ -0,0 +1,18 @@
+# Script para visualizar el GeoTIFF de trayectorias
+# Proyecto: Premio MapBiomas Bolivia 2025
+# Autora: Neri
+
+# ============================
+# Cargar paquetes necesarios
+# ============================
+
+library(terra)
+library(sf)
+library(tmap)
+library(RColorBrewer)
+
+# ============================
+# Aquí cargaremos el archivo GeoTIFF cuando esté listo
+# ============================
+
+# Aquí va el código más adelante...
