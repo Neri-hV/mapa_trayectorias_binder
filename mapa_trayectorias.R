e74f07109e2a926e4bc64422419f90cc34433464
Coco <91480391+Neri-hV@users.noreply.github.com>
2025-08-08 12:22:06 -0400

    Update mapa_trayectorias.R

--- mapa_trayectorias.R
+++ mapa_trayectorias.R
@@ -1,18 +1,20 @@
 # Script para visualizar el GeoTIFF de trayectorias
-# Proyecto: Premio MapBiomas Bolivia 2025
 # Autora: Neri
 
 # ============================
 # Cargar paquetes necesarios
 # ============================
-
+# Descargar y procesar el .tif desde Drive
 library(terra)
-library(sf)
-library(tmap)
-library(RColorBrewer)
+library(googledrive)
 
-# ============================
-# Aquí cargaremos el archivo GeoTIFF cuando esté listo
-# ============================
+# Enlace directo (usa el ID de tu archivo)
+url <- "https://drive.google.com/file/d/1nrTrNEbUETik1p3zPDiTidGLeaaxsxwR/view?usp=sharing"
+download.file(url, "trayectorias_bosque_agro_bidireccional.tif", mode = "wb")  # "wb" es clave para archivos binarios
+
+# Cargar y visualizar
+raster_gee <- rast("trayectorias_bosque_agro_bidireccional.tif")
+plot(raster_gee, main = "Imagen desde GEE")
+
 
 # Aquí va el código más adelante...
