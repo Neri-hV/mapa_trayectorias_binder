54db399773c2ef45aefebd1020bae0a668afe518
Coco <91480391+Neri-hV@users.noreply.github.com>
2025-08-08 12:42:31 -0400

    Update mapa_trayectorias.R

--- mapa_trayectorias.R
+++ mapa_trayectorias.R
@@ -4,17 +4,23 @@
 # ============================
 # Cargar paquetes necesarios
 # ============================
-# Descargar y procesar el .tif desde Drive
-library(terra)
 library(googledrive)
+library(raster)
+library(ggplot2)
 
-# Enlace directo (usa el ID de tu archivo)
-url <- "https://drive.google.com/file/d/1nrTrNEbUETik1p3zPDiTidGLeaaxsxwR/view?usp=sharing"
-download.file(url, "trayectorias_bosque_agro_bidireccional.tif", mode = "wb")  # "wb" es clave para archivos binarios
+# 1. Descargar imagen desde Drive
+url <- "https://drive.google.com/file/d/1nrTrNEbUETik1p3zPDiTidGLeaaxsxwR/view?usp=drive_link"
+download.file(url, "trayectorias_bosque_agro_bidireccional.tif", mode = "wb")
 
-# Cargar y visualizar
-raster_gee <- rast("trayectorias_bosque_agro_bidireccional.tif")
-plot(raster_gee, main = "Imagen desde GEE")
+# 2. Cargar con raster (compatible universal)
+raster_gee <- raster("trayectorias_bosque_agro_bidireccional.tif")
+
+# 3. Visualizar con ggplot2
+ggplot() +
+  layer_raster(raster_gee) +
+  scale_fill_viridis_c() +
+  labs(title = "Imagen desde GEE") +
+  theme_minimal()
 
 
 # Aquí va el código más adelante...
