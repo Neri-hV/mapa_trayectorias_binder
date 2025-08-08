b3daa4b7e2ebf4dbadadda5dea67241f33ea484b
Coco <91480391+Neri-hV@users.noreply.github.com>
2025-08-08 14:28:23 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,8 +1,10 @@
 # Paquetes principales para GIS y visualización
 install.packages("sf")
 install.packages("sp")
+install.packages("tmap")
 install.packages("terra")
-install.packages("raster")
+install.packages("rcolorbrewer")
+install.packages("dplyr")
 install.packages("googledrive")
 install.packages("ggplot2")
 
