aacb8586b9b084873e74cf589e29866682f9119b
Coco <91480391+Neri-hV@users.noreply.github.com>
2025-08-13 14:11:47 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,6 @@
 # Paquetes principales para GIS y visualización
-install.packages("sp")
-install.packages("RColorBrewer")
+install.packages("tidyverse")
+install.packages("readr")
 install.packages("dplyr")
 install.packages("googledrive")
 install.packages("ggplot2")
-# Reinstalar paquetes problemáticos
-install.packages(c("sf", "terra", "tmap"), 
-                 configure.args = c("--with-proj-lib=/usr/lib"))
